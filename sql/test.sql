e4ddd6297d1862ac268333e6d76c1de2a0f3349e
Peter Mwendia <ptermwendia@gmail.com>
2023-09-29 03:26:38 +0300

    Create test.sql

---
+++ sql/test.sql
@@ -0,0 +1,19 @@
+-- Active: 1695896087963@@127.0.0.1@3306@LittleLemonDB
+
+SELECT * FROM Customers;
+
+
+SELECT * FROM Menu;
+
+
+SELECT * FROM Orders;
+
+
+SELECT * FROM Bookings;
+
+
+SELECT * FROM OrderDeliveryStatus;
+
+
+SELECT * FROM StaffInformation;
+
